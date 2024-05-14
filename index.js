f02e79906234db65a7467ebe6f801e1b46adf59b
Stéphane RAVET <ravet.st@gmail.com>
2024-05-14 11:42:42 +0200

    Add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 // Configuration de base pour le module ES
 const __filename = fileURLToPath(import.meta.url);
@@ -11,6 +12,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 let photos = [];
